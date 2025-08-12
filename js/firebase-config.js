3d6b9d0da8f83e7d2f934293c9ebc09441ae6e20
Mythomathic <octoctoctoctoctoct1@gmail.com>
2025-08-12 18:21:34 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDPVUQZXaSAARwROsDgvlohMvB-esYvMrA",
+  authDomain: "mythomathic.firebaseapp.com",
+  projectId: "mythomathic",
+  storageBucket: "mythomathic.firebasestorage.app",
+  messagingSenderId: "396574593247",
+  appId: "1:396574593247:web:5b7f02c53de5613eda996a",
+  measurementId: "G-P649F73C8E"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = firebase.auth();
