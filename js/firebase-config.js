01cf793cb64ccbe04c1c2b7c7eb309eae367cb3a
Mythomathic <octoctoctoctoctoct1@gmail.com>
2025-08-15 13:28:45 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyDPVUQZXaSAARwROsDgvlohMvB-esYvMrA",
   authDomain: "mythomathic.firebaseapp.com",
   projectId: "mythomathic",
@@ -15,4 +15,29 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = firebase.auth();
+const auth = firebase.auth(); */
+
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDPVUQZXaSAARwROsDgvlohMvB-esYvMrA",
+    authDomain: "mythomathic.firebaseapp.com",
+    projectId: "mythomathic",
+    storageBucket: "mythomathic.firebasestorage.app",
+    messagingSenderId: "396574593247",
+    appId: "1:396574593247:web:5b7f02c53de5613eda996a",
+    measurementId: "G-P649F73C8E"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
